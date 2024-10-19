a5fd99c282a91ec4c297a9722be0c744e3281de0
eyobScript <eyobbmulugeta@gmaol.com>
2024-10-19 10:24:41 +0300

    dDataProvider';

---
+++ amazon-clone/src/Components/Product/Product.jsx
@@ -0,0 +1,35 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import ProductCard from './ProductCard';
+import classes from './product.module.css'
+import Loader from '../Loader/Loader';
+
+function Product() {
+    const [Products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+      setLoading(true);   
+      axios.get('https://fakestoreapi.com/products')
+      .then((response) => {
+        setProducts(response.data);
+        setLoading(false);  
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+    },[])
+  return (
+     <>
+    {
+      loading ? <Loader /> : (<section className={classes.products_container}>
+        {
+            Products.map((product) => <ProductCard key={product.id} product={product}/>)
+        }
+    </section>)
+    }
+    </>
+  )
+}
+
+export default Product
