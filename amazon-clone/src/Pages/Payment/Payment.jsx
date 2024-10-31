8e14516ec7c7ef4748e2c154c69b8734ac25c0ad
eyobScript <eyobbmulugeta@gmaol.com>
2024-10-31 15:40:20 +0300

    payment  styles

--- amazon-clone/src/Pages/Payment/Payment.jsx
+++ amazon-clone/src/Pages/Payment/Payment.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import LayOut from '../../Components/LayOut/LayOut'
 import classes from './payment.module.css'
+import { DataProvider } from '../../ContextProvider/ContextProvider'
 
 function Payment() {
+  const [{user,basket}] = useContext(DataProvider);
+  const totalItems = basket?.reduce((amount, item) => {
+      return amount + item.amount;
+    }, 0);
   return (
     <LayOut>
       {/*  Header  */}
-      <div className={classes.payment_header}>Checkout (2) items</div>
+      <div className={classes.payment_header}>Checkout ({totalItems}) items</div>
       {/* payment method */}
-      <section>
+      <section className={classes.payment}>
         {/* address */}
-        <div></div>
+        <div className={classes.flex}>
+          <h3>Delivery Address</h3>
+          <div>
+            <div>{user.email}</div>
+            <div>123 React lane</div>
+            <div>Ethiopia, addis</div>
+          </div>
+        </div>
         <hr />
         {/* product */}
         <div></div>
