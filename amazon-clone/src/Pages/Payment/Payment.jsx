db284d4efa4bf0b8b9b137754880b87618b509a8
eyobScript <eyobbmulugeta@gmaol.com>
2024-10-31 15:20:25 +0300

    Initialized payment method

--- amazon-clone/src/Pages/Payment/Payment.jsx
+++ amazon-clone/src/Pages/Payment/Payment.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import LayOut from '../../Components/LayOut/LayOut'
+import classes from './payment.module.css'
 
 function Payment() {
   return (
     <LayOut>
+      {/*  Header  */}
       <h1>payment</h1>
+      {/* payment method */}
     </LayOut>
   )
 }
