224588fa7d77cb05c2cc73893c33e8ba5a680405
eyobScript <eyobbmulugeta@gmaol.com>
2024-11-15 19:23:10 +0300

    structure of order

--- amazon-clone/src/Pages/Orders/Orders.jsx
+++ amazon-clone/src/Pages/Orders/Orders.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 import LayOut from '../../Components/LayOut/LayOut';
+import classes from './order.module.css'
+import { db } from '../../Utility/firebase';
+import { DataProvider } from '../../ContextProvider/ContextProvider'
 
 function Orders() {
+
+  const [{user}, dispatch] = useContext(DataProvider);
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    // fetch orders from db
+    if(user){
+      db.collection('users')
+      .doc(user.uid)
+      .collection('orders')
+      .orderBy('created','desc')
+      .onSnapshot((snapshot) => {
+        // console.log(snapshot)
+        setOrders(snapshot.docs.map((doc) => doc.data()));
+      });
+    }else{}
+  }, []);
   return (
     <LayOut>
-      <h1>order</h1>
+      <section className={classes.container}>
+        <div className={classes.order_container}>
+          <h2>Your Orders</h2>
+          {/* ordered items */}
+          <div>
+
+          </div>
+        </div>
+      </section>
     </LayOut>
   )
 }
