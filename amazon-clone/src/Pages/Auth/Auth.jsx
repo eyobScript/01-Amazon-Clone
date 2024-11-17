fb32165e7f689b23097a2ee66652dc98e9c55315
eyobScript <eyobbmulugeta@gmaol.com>
2024-11-17 15:44:25 +0300

    some changes

--- amazon-clone/src/Pages/Auth/Auth.jsx
+++ amazon-clone/src/Pages/Auth/Auth.jsx
@@ -1,24 +1,25 @@
-import React,{useState,useContext} from 'react';
-import { Link, useNavigate,useLocation } from 'react-router-dom';
-import classes from './Signup.module.css';
-import {auth} from '../../Utility/firebase';
-import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
-import { DataProvider } from '../../ContextProvider/ContextProvider';
-import { Type } from '../../Utility/action.type';
-import { ClipLoader } from 'react-spinners'
-
-
+import React, { useState, useContext } from "react";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import classes from "./Signup.module.css";
+import { auth } from "../../Utility/firebase";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import { DataProvider } from "../../ContextProvider/ContextProvider";
+import { Type } from "../../Utility/action.type";
+import { ClipLoader } from "react-spinners";
 
 function Auth() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const [loading, setLoading] = useState({
     signIn: false,
     signUp: false,
   });
 
-  const [{user}, dispatch ] = useContext(DataProvider);
+  const [{ user }, dispatch] = useContext(DataProvider);
   const navigate = useNavigate();
   const navStateData = useLocation();
 
@@ -26,78 +27,107 @@ function Auth() {
     e.preventDefault();
     try {
       if (e.target.name === "signin") {
-        setLoading({...loading, signIn:true});
-        const userCredential = await signInWithEmailAndPassword(auth, email, password);
+        setLoading({ ...loading, signIn: true });
+        const userCredential = await signInWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
         dispatch({
-          type: Type.SET_USER, 
-          user: userCredential.user 
+          type: Type.SET_USER,
+          user: userCredential.user,
         });
-        setLoading({...loading, signIn:false});
-        navigate(navStateData?.state?.redirect || '/');
+        setLoading({ ...loading, signIn: false });
+        navigate(navStateData?.state?.redirect || "/");
       } else {
-        setLoading({...loading, signUp:true});
-        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+        setLoading({ ...loading, signUp: true });
+        const userCredential = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
         dispatch({
-          type: Type.SET_USER, 
-          user: userCredential.user
+          type: Type.SET_USER,
+          user: userCredential.user,
         });
-        setLoading({...loading, signUp:false});
-        navigate(navStateData?.state?.redirect || '/');
+        setLoading({ ...loading, signUp: false });
+        navigate(navStateData?.state?.redirect || "/");
       }
     } catch (error) {
       setError(error.message);
     }
   }
-  
 
   return (
     <section className={classes.login}>
-      <Link to='/'>
-      <img src="https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg" alt="" />
+      <Link to="/">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg"
+          alt=""
+        />
       </Link>
       <div className={classes.login_container}>
         <h1>Sign In</h1>
         {navStateData.state.msg && (
-          <small style={{
-            padding: '5px',
-            textAlign: 'center',
-            color:'red',
-            fontWeight: 'bold'
-          }}>
-            {navStateData.state.msg}
+          <small className={classes.small_popUp}>
+            {navStateData?.state?.msg}
           </small>
-          )
-          
-        }
+        )}
         <form action="">
           <div>
             <label htmlFor="email">Email</label>
-            <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" id='email'/>
+            <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              id="email"
+            />
           </div>
           <div>
             <label htmlFor="password">Password</label>
-            <input  value={password} onChange={(e) => setPassword(e.target.value)} type="password" id='password'/>
+            <input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              id="password"
+            />
           </div>
-          <button type='submit' name='signin' onClick={authHandler} className={classes.login_signInButton}>
-            {
-              loading.signIn ? <ClipLoader size={20} color="#123abc" /> :
+          <button
+            type="submit"
+            name="signin"
+            onClick={authHandler}
+            className={classes.login_signInButton}
+          >
+            {loading.signIn ? (
+              <ClipLoader size={20} color="#123abc" />
+            ) : (
               "Sign In"
-            }
+            )}
           </button>
         </form>
         <p>
-          By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use & Sale. Please See Our Privacy Notice, our Cookies Noting and our Interest-Based Ads Notice.
+          By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use &
+          Sale. Please See Our Privacy Notice, our Cookies Noting and our
+          Interest-Based Ads Notice.
         </p>
-        <button type='submit' name='signup' onClick={authHandler} className={classes.login_registerButton}>
-          {
-            loading.signUp ? <ClipLoader size={20} color="#123abc" /> :
+        <button
+          type="submit"
+          name="signup"
+          onClick={authHandler}
+          className={classes.login_registerButton}
+        >
+          {loading.signUp ? (
+            <ClipLoader size={20} color="#123abc" />
+          ) : (
             "Create Account"
-          }
+          )}
         </button>
-        {error && <small style={{marginTop: '5px', color:'red'}}>{error}</small>}
+        {error && (
+          <small style={{ marginTop: "5px", color: "red" }}>{error}</small>
+        )}
       </div>
     </section>
-  )
+  );
 }
 
-export default Auth
+export default Auth;
